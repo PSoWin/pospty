cfcdb2063aaf1689cb8a2a990b5f662f198d9f73
TitanSnow <sweeto@live.cn>
2017-07-21 00:52:17 +0800

    add docstrings

--- pospty.pyx
+++ pospty.pyx
@@ -26,28 +26,39 @@ cdef extern from '<sys/ioctl.h>':
     enum: TIOCSWINSZ
 
 cdef void* safe_malloc(size_t size) except NULL:
+    """safe malloc
+    raise MemoryError if failed"""
     cdef void* ptr = malloc(size)
     if ptr == NULL:
         raise MemoryError('malloc failed')
     return ptr
 
 cdef void* safe_calloc(size_t nmemb, size_t size) except NULL:
+    """safe calloc
+    raise MemoryError if failed"""
     cdef void* ptr = calloc(nmemb, size)
     if ptr == NULL:
         raise MemoryError('calloc failed')
     return ptr
 
 class PosixError(OSError):
+    """error of system"""
     def __init__(self, errno, strerror, callname):
+        """init error with
+        ``errno``
+        ``strerror`` - textual error msg gets from ``strerror()``
+        ``callname`` - what system call caused error"""
         super().__init__(errno, strerror, callname)
         self.callname = callname
     @classmethod
     def from_errno(cls, callname, errnum=None):
+        """create PosixError by ``callname`` and last errno (if errnum is None) or errnum"""
         if errnum is None:
             errnum = errno
         return cls(errnum, (<bytes>strerror(errnum)).decode(), callname)
     @classmethod
     def raise_errno(cls, callname, errnum=None):
+        """raise PosixError by ``callname`` and last errno (if errnum is None) or errnum"""
         raise cls.from_errno(callname, errnum)
 
 cdef class Config:
@@ -61,21 +72,25 @@ cdef class Config:
         free(self._termios)
         free(self._winsize)
     def _alloc(self):
+        """alloc memory for ``self._termios`` and ``self._winsize``"""
         self._termios = <termios*>safe_calloc(1, sizeof(termios))
         self._winsize = <winsize*>safe_calloc(1, sizeof(winsize))
-    @classmethod
-    def default(cls):
-        cdef Config obj = cls.__new__(cls)
+    @staticmethod
+    def default():
+        """create default Config"""
+        cdef Config obj = Config.__new__(Config)
         return obj
-    @classmethod
-    def _from_fd(cls, fd):
-        cdef Config obj = cls.__new__(cls)
+    @staticmethod
+    def _from_fd(fd):
+        """create same Config as ``fd`` has"""
+        cdef Config obj = Config.__new__(Config)
         obj._alloc()
         if tcgetattr(fd, obj._termios) == -1:
             PosixError.raise_errno('tcgetattr')
         if ioctl(fd, TIOCGWINSZ, obj._winsize) == -1:
             PosixError.raise_errno('ioctl')
         return obj
-    @classmethod
-    def from_file(cls, f):
-        return cls._from_fd(f.fileno())
+    @staticmethod
+    def from_file(f):
+        """create same Config as file ``f`` has"""
+        return Config._from_fd(f.fileno())
